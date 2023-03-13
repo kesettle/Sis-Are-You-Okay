c9236943bdec38674c3bd4398e0a373457d5f31c
Bennett McAuley <bennettmcauley@gmail.com>
2023-03-12 22:29:20 -0400

    Data filtering for preliminary analysis

--- data cleaning.R
+++ data cleaning.R
@@ -3,8 +3,8 @@
 library(tidyverse)
 library(haven)
 
-#read in SAV file, allow file choice (for now)
-dat <- read_sav(choose.files())
+#reading in csv version of the data, loading from local drive
+dat <- read.csv(choose.files())
 
 #structure of data and variables
 str(dat)
@@ -18,3 +18,21 @@ str(dat)
 screenDat <- filter(dat, Race_AA == 1, Screening_Female == 1, Screening_Age == 1, Screening_Consent == 1)
 
 ## Variables: What to remove/include? Recode vs Original?
+
+
+# Removing all definitely unneeded and open-ended response variables (i.e., end with "_text") 
+
+closedDat <- screenDat %>% select(!c((StartDate:IPAddress),
+                                    (RecordedDate:Covid_Wellbeing_Financial),
+                                    (Followed_Protests:Protests_police_good_job),
+                                    (Prior_Covid_experienced_racial_discrimination:GAD7_Q7_afraid_something_bad),
+                                    Coping_other,
+                                    Employment_status_other,
+                                    Zip,
+                                    (Focus_group_interview:id),
+                                    ends_with("_text")))
+
+#Filtering for those who completed at least half of the survey; only 207 didn't make it that far
+#and the smallest value is 66%, which I think is enough...for now
+
+complDat <- closedDat %>% filter(Progress > 50)
