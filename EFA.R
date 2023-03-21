dee1d71c0321306bb35bb2958317376c6691d46c
Bennett McAuley <bennettmcauley@gmail.com>
2023-03-21 17:48:31 -0400

    Updated to obtaining loadings for factors

--- EFA.R
+++ EFA.R
@@ -1,15 +1,25 @@
 #----Install and/or load needed libraries----#
 install.packages("nFactors")
+install.packages("semPlot")
+install.packages("GPArotation")
 library(nFactors)
+library(semPlot)
+library(GPArotation)
 library(tidyverse)
 
 
 #-----Find principal components-----#
-PCs <- prcomp(~ ., data = okDat, scale = TRUE)
+#Response(s) must be removed
+pcDat <- okDat[,-c(34,67)]
+
+PCs <- prcomp(~ ., data = pcDat, scale = TRUE)
 summary(PCs)
 
-par(mfrow = c(1, 2))
 
+#Contribution of variance levels out at around 4 to 5 PCs; may consider no more than that
+plot(PCs, type = 'l')
+
+par(mfrow = c(1, 2))
 plot(PCs$sdev^2/sum(PCs$sdev^2), xlab = "Principal Component",
      ylab = "Proportion of Variance Explained", ylim = c(0, 1), type = 'b')
 plot(cumsum(PCs$sdev^2/sum(PCs$sdev^2)), xlab = "Principal Component",
@@ -17,16 +27,32 @@ plot(cumsum(PCs$sdev^2/sum(PCs$sdev^2)), xlab = "Principal Component",
 
 
 #-----Estimate number of factors to retain with scree test-----#
-#15, it seems
-okScr <- nScree(okDat)
+#15 seems to be the majority
+okScr <- nScree(pcDat)
 okScr
 
 #Examine the eigenvalues
-#The first 15 are > 1.0, so this suggests about 15 factors; the first 5 >> 1.0
-round(eigen(cor(okDat))$values, 4)
+
+#The first 15 are > 1.0, so this suggests about 15 factors; the first 5 >> 1.0,
+#which is consistent with the PCA
+round(eigen(cor(pcDat))$values, 4)
 
 
-#-----Estimate EFA model with k = 15 factors-----#
-factanal(okDat, factors = 15)
+#-----Estimate and Compare EFA models with k = 5 vs 15 factors-----#
 
+okEFA5 <- factanal(pcDat, factors = 5, scores = "regression", rotation = "oblimin")
+okEFA15 <- factanal(pcDat, factors = 15, scores = "regression", rotation = "oblimin")
+
+
+#Chi-square test for model differences:
+#H0: Chisq5 - Chisq15 = 0; both models fit equally well, so smaller model is sufficient
+#HA: Chisq 5 - Chisq15 =/= 0; larger model fits data better than smaller model
+
+chi_diff <- okEFA5$STATISTIC - okEFA15$STATISTIC
+df_diff <- okEFA5$dof - okEFA15$dof
+
+
+chi <- qchisq(0.05, df_diff, lower.tail = FALSE)
+
+print(okEFA15$loadings, sort = TRUE, cutoff = 0.2)
 
